9766ba92c318528f76d0a55043b976804c246912
lhoangphuong <49200808+lhoangphuong@users.noreply.github.com>
2022-11-11 00:20:53 +0700

    delete folder

--- nvim/init.lua
+++
@@ -1,8 +0,0 @@
---[[ init.lua ]]
-require('settings')     -- lua/settings.lua
-require('plugins')      -- lua/plugins.lua
-require('keys')         -- lua/keys.lua
-
-
--- Plugins list
-require('nvim_tree')
