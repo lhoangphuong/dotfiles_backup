b469d8147a4bf4bc33639a197ffae238cb939f7f
plh <phuong.le@elca.vn>
2022-11-10 18:52:20 +0700

    Add neovim config

---
+++ nvim/init.lua
@@ -0,0 +1,8 @@
+--[[ init.lua ]]
+require('settings')     -- lua/settings.lua
+require('plugins')      -- lua/plugins.lua
+require('keys')         -- lua/keys.lua
+
+
+-- Plugins list
+require('nvim_tree')`
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,57 @@
+local nvim_tree = require('nvim-tree')
+local gwidth = vim.api.nvim_list_uis()[1].width
+local gheight = vim.api.nvim_list_uis()[1].height
+local width = 60
+local height = 20
+
+
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+      fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+      vim.cmd [[packadd packer.nvim]]
+      return true
+    end
+    return false
+  end
+  
+  local packer_bootstrap = ensure_packer()
+  
+  return require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim'
+    -- My plugins here
+    -- use 'foo1/bar1.nvim'
+    -- use 'foo2/bar2.nvim'
+    
+    -- Themes
+    use({
+        'Mofiqul/vscode.nvim',
+        as = 'vscode',
+        config = function()
+          vim.cmd('colorscheme vscode')
+        end
+    })
+
+
+    -- File explorer
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = { 'nvim-tree/nvim-web-devicons'}
+    }
+
+
+    -- Status line
+    use {
+      'nvim-lualine/lualine.nvim',
+      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
+    require('lualine').setup()
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+      require('packer').sync()
+    end
+  end)
+
