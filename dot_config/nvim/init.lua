ed72020796217149d8d0ab3e95265712f35ddafb
plh <phuong.le@elca.vn>
2022-11-11 10:41:54 +0700

    update dotfiles

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,12 +1,12 @@
 --[[ init.lua ]]
-require('settings')             -- lua/settings.lua
-require('keys')                 -- lua/keys.lua
-
-
--- Custom
-require('plugins_config.packer')
+require('plugins_config.packer') -- Plugin manager need to be loaded first
+require('plugins_config.telescope') -- Plugin with many dependency load second :v
 require('plugins_config.nvim_tree')
 require('plugins_config.lualine')
 require('plugins_config.bufferline')
-require('plugins_config.telescope')
+
+
+-- Sytem wide config
+require('settings')  
+require('keys')      
 vim.cmd('colorscheme vscode')   -- Choose theme
