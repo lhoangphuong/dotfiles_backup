f8893a2bc76200f049c606e602d17c936a64bba7
plh <phuong.le@elca.vn>
2022-11-11 10:37:28 +0700

    update dotfiles

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -8,4 +8,5 @@ require('plugins_config.packer')
 require('plugins_config.nvim_tree')
 require('plugins_config.lualine')
 require('plugins_config.bufferline')
+require('plugins_config.telescope')
 vim.cmd('colorscheme vscode')   -- Choose theme
